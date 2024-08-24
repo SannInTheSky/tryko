4c26abe085a1ec3b2b235096309ddc744ecd2fdc
SannInTheSky <202380082@psu.palawan.edu.ph>
2024-08-24 14:55:01 +0800

    message

---
+++ CornelGuevarraPS02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CornelGuevarraPS02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even Numbers from 1-20: ");
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# tryko
+# Practice Sets 02
+
+## Activities
+- Write a program that prints all the even numbers from 1 to 20 using a for loop.
+- Write a program that prints all the odd numbers from 1 to 20 using a for loop.
+
+## Authors
+
+
